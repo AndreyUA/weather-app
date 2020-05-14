0fc92093e2e390b1dec4f7f1a4f673a026b2d3c0
AndreyUA <infernalsteel666@gmail.com>
2020-05-14 17:29:00 +0300

    Updates

---
+++ precache-manifest.6b167a6702da45942cd3fa547a061669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f58f61378084187d3d684463a1bd96",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "25ab2fb97c6bc62d621d",
+    "url": "/weather-app/static/css/main.5706d09b.chunk.css"
+  },
+  {
+    "revision": "9f8b865cadc33b93aa91",
+    "url": "/weather-app/static/js/2.08b3e3fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.08b3e3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ab2fb97c6bc62d621d",
+    "url": "/weather-app/static/js/main.f296a55e.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  }
+]);
